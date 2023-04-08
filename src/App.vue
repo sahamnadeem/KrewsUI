3446f988c73549cccecb3543162f61a5769aa25b
Saham Nadeem <apple@192.168.10.8>
2023-04-08 17:05:42 +0500

    installed vuetify & created navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app id="inspire">
+    <nav-bar :links="links"></nav-bar>
+    <router-view />
+  </v-app>
+
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  components:{
+    NavBar,
+  },
+  data() {
+    return {
+      links: ["Dashboard", "Messages", "Profile", "Updates"],
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
